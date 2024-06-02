49b7c9b09b1371c280b6b3e1aa470375f1c86546
Esteban Palacio <estebanpalacio0625@gmail.com>
2024-06-02 18:34:32 -0500

    Parte final del proyecto
    Leer y mostrar datos.
    Borrar registros.
    Estilos y ajustes.

--- todolist/routes/web.php
+++ todolist/routes/web.php
@@ -2,17 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
-<<<<<<< Updated upstream
-/*
-Route::get('/', function () {
-    return view('task.index');
-});
-*/
 
-Route::get('/', [TaskController::class,'index']);
-Route::post('/', [TaskController::class,'store']);
-
-=======
 
 // Route::get('/', function () {
 //     return view('task.index');
@@ -21,4 +11,4 @@ Route::post('/', [TaskController::class,'store']);
 Route::get('/', [TaskController::class,'index']);
 Route::post('/', [TaskController::class,'store']);
 Route::delete('/{id}', [TaskController::class,'destroy'])->name('task.destroy');
->>>>>>> Stashed changes
+
